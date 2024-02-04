d5b277bd563f33a39a7cd440e745ef99e8f738f0
Andrea Wimberská <awimberska@gmail.com>
2024-02-04 19:25:58 +0100

    feat: created Results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import Results from './components/Results';
+import { useState } from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 10,
+    duration: 10,
+  });
+
+  const validData = userInput.duration >= 1;
+
+  const handleInput = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: +newValue };
+      //+ transform string to number
+    });
+  };
+
   return (
     <>
       <Header />
-      <UserInput/>
+      <UserInput userInput={userInput} onChangeInput={handleInput} />
+      {!validData && <p className='center'>Please enter the duration greater than 0.</p>}
+      {validData &&<Results input={userInput} />}
     </>
   );
 }
